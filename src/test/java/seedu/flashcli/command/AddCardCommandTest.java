e7912936d295c4962f61772bc581a61e662d1832
Advait Bagri <advait.bagri@gmail.com>
2026-03-19 12:47:53 +0800

    AddCardCommandTest: Fix AddCardCommand test to use AddCardArgs constructor and Card getters

--- src/test/java/seedu/flashcli/command/AddCardCommandTest.java
+++ src/test/java/seedu/flashcli/command/AddCardCommandTest.java
@@ -3,6 +3,7 @@ package seedu.flashcli.command;
 import org.junit.jupiter.api.Test;
 import seedu.flashcli.deck.DeckManager;
 import seedu.flashcli.exception.FlashException;
+import seedu.flashcli.parser.AddCardArgs;
 import seedu.flashcli.ui.Ui;
 
 import java.util.Scanner;
@@ -20,13 +21,13 @@ public class AddCardCommandTest {
         Command create = new CreateDeckCommand("MA1513");
         create.execute(deckManager, ui, scanner);
 
-        Command add = new AddCardCommand("MA1513", "1+1", "2");
+        Command add = new AddCardCommand(new AddCardArgs("MA1513", "1+1", "2"));
 
         assertFalse(add.execute(deckManager, ui, scanner));
 
-        assertEquals("Question: 1+1",
-                deckManager.getDeck("MA1513").getCard(0).getQuestionString());
-        assertEquals("Answer: 2",
-                deckManager.getDeck("MA1513").getCard(0).getAnswerString());
+        assertEquals("1+1",
+                deckManager.getDeck("MA1513").getCard(0).getQuestion());
+        assertEquals("2",
+                deckManager.getDeck("MA1513").getCard(0).getAnswer());
     }
 }
