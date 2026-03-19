67a477af57afef89b73f4a03c8ce9f3f5cf44fe0
Sid3024 <siddharth.arum@gmail.com>
2026-03-19 20:12:39 +0800

    Fix code style issues in command test classes

--- src/test/java/seedu/flashcli/command/AddCardCommandTest.java
+++ src/test/java/seedu/flashcli/command/AddCardCommandTest.java
@@ -9,24 +9,19 @@ import seedu.flashcli.ui.Ui;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddCardCommandTest {
-
-
     @Test
-    public void execute_validCard_cardAddedToDeck() {
+    public void execute_validCard_cardAddedToDeck() throws FlashException{
         DeckManager deckManager = new DeckManager();
         Ui ui = new Ui();
-        try {
-            Command commandCreateDeck = new CreateDeckCommand("MA1513");
-            commandCreateDeck.execute(deckManager, ui);
-            AddCardArgs addCardArgs = new AddCardArgs("MA1513", "1+1", "2");
-            Command commandAddCard = new AddCardCommand(addCardArgs);
-            assertFalse(commandAddCard.execute(deckManager, ui));
-            assertEquals("Question: 1+1",deckManager.getDeck("MA1513").getCard(0).getQuestionString());
-            assertEquals("Answer: 2",deckManager.getDeck("MA1513").getCard(0).getAnswerString());
-            assertEquals(1, deckManager.getDeck("MA1513").getSize());
-        } catch (FlashException e) {
-            System.out.println(e.getMessage());
-        }
+        Command commandCreateDeck = new CreateDeckCommand("MA1513");
+        commandCreateDeck.execute(deckManager, ui);
+        AddCardArgs addCardArgs = new AddCardArgs("MA1513", "1+1", "2");
+        Command commandAddCard = new AddCardCommand(addCardArgs);
+        assertFalse(commandAddCard.execute(deckManager, ui));
+        assertEquals("Question: 1+1",deckManager.getDeck("MA1513").getCard(0).getQuestionString());
+        assertEquals("Answer: 2",deckManager.getDeck("MA1513").getCard(0).getAnswerString());
+        assertEquals(1, deckManager.getDeck("MA1513").getSize());
+
 
     }
 
--- src/test/java/seedu/flashcli/command/CreateDeckCommandTest.java
+++ src/test/java/seedu/flashcli/command/CreateDeckCommandTest.java
@@ -9,16 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CreateDeckCommandTest {
     @Test
-    public void execute_validDeck_deckCreated() {
+    public void execute_validDeck_deckCreated() throws FlashException {
         DeckManager deckManager = new DeckManager();
         Ui ui = new Ui();
-        try {
-            Command command = new CreateDeckCommand("MA1513");
-            assertFalse(command.execute(deckManager, ui));
-            assertNotNull(deckManager.getDeck("MA1513"));
-        } catch (FlashException e) {
-            System.out.println(e.getMessage());
-        }
+        Command command = new CreateDeckCommand("MA1513");
+        assertFalse(command.execute(deckManager, ui));
+        assertNotNull(deckManager.getDeck("MA1513"));
+
     }
 
 }
